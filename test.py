f3ec56d1c9a7b37182ce88b4519c079ccf0fb530
Ярослав Кальвияйнен <yar.slav.ka2002@ya.ru>
2023-02-24 11:37:02 +0300

    Coverage fix try

--- test.py
+++ test.py
@@ -24,4 +24,40 @@ def test_golden(golden, caplog):
         with open(result_file, 'r') as f:
             result = f.readlines()
 
-        assert result == golden.out['output']
+        assert result == golden.out['output1']
+
+
+@pytest.mark.golden_test('tests/*.yml')
+def test_golden2(golden, caplog):
+    with tempfile.TemporaryDirectory() as tmp_dir:
+        import compiler
+        import procrssor_interface
+        import cu
+
+        output_file: str = os.path.join(tmp_dir, 'compiled.bin')
+        input_file: str = os.path.join(tmp_dir, 'input')
+        result_file: str = os.path.join(tmp_dir, 'result')
+        compiler.compile(golden['input_file'], output_file)
+
+        if golden['input_data'] is not None:
+            control_unit: cu.ControlUnit = cu.ControlUnit()
+
+            procrssor_interface.load_program(output_file, control_unit)
+
+            nums: list = str(golden['input_data']).split(',')
+            for num in nums:
+                procrssor_interface.add_input(int(num, 0), control_unit)
+
+            trace, ticks = control_unit.work()
+
+            result = [control_unit.proc.output_queue, ticks, trace]
+        else:
+            control_unit: cu.ControlUnit = cu.ControlUnit()
+
+            procrssor_interface.load_program(output_file, control_unit)
+
+            trace, ticks = control_unit.work()
+
+            result = [control_unit.proc.output_queue, ticks, trace]
+
+        assert result == golden.out['output2']
